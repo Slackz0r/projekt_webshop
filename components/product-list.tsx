01be941c36a2f8f7af36fa1796819691b064bc4a
Slackz0r <gustav.aldenberg@gmail.com>
2026-03-11 16:28:06 +0100

    fetch limit and product-list/stock-counter styling

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,31 +1,45 @@
-import { Product } from "@/app/types";
+import { Product, Category } from "@/app/types";
 
-export default async function ProductList({ products }: { products: Product[] }) {
+type ProductListProps = {
+  products: Product[];
+  categories: Category[];
+};
+
+export default function ProductList({ products, categories }: ProductListProps) {
+  const categoryMap = new Map(categories.map((category) => [category.id, category.name]));
+
+  console.log(categoryMap);
+
   return (
     <table>
-      <tr>
-        <td>Product</td>
-        <td>Category</td>
-        <td>Price</td>
-        <td>Stock</td>
-        <td>Status</td>
-        <td>Edit</td>
-        <td>Delete</td>
-      </tr>
-      {products.map(
-        ({ id, title, sku, categoryId, price, stock, availabilityStatus, thumbnail }) => (
-          <tr key={id}>
-            <td className="grid">
-              <span>{title}</span>
-              <span>SKU: {sku}</span>
-            </td>
-            <td>{categoryId}</td>
-            <td>{price}</td>
-            <td>{stock}</td>
-            <td>{availabilityStatus}</td>
-          </tr>
-        ),
-      )}
+      <thead>
+        <tr>
+          <th>Product</th>
+          <th>Category</th>
+          <th>Price</th>
+          <th>Stock</th>
+          <th>Status</th>
+          <th>Edit</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {products.map(
+          ({ id, title, sku, categoryId, price, stock, availabilityStatus, thumbnail }) => (
+            <tr key={id}>
+              <td className="grid">
+                <span>{title}</span>
+                <span>SKU: {sku}</span>
+              </td>
+              <td>{categoryMap.get(categoryId) ?? "Unknown"}</td>
+              <td>{price}</td>
+              <td>{stock}</td>
+              <td>{availabilityStatus}</td>
+            </tr>
+          ),
+        )}
+      </tbody>
     </table>
     // <section>
     //   {products.map(
